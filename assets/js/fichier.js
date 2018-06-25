6f3fd6a160ae1d22234f46bbe1d80303323a715e
massa <massafranck@gmail.com>
2018-06-25 18:45:44 +0200

    corrigé

---
+++ assets/js/fichier.js
@@ -0,0 +1,6 @@
+//fonction qui permet de changer l'image au survol
+ function imgChange(img){
+  //Afiichage des photos en survolant
+  document.getElementById(img).src = 'assets/img/' + img + '_2.jpg' ;
+
+}
